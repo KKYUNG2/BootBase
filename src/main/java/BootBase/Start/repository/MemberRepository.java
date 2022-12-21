82259c58fd4bdcb6cd9fa036c621b3aabbbd3ccb
KKYUNG2 <backender9615@gmail.com>
2022-12-21 23:54:31 +0900

    Object

---
+++ src/main/java/BootBase/Start/repository/MemberRepository.java
@@ -0,0 +1,16 @@
+package BootBase.Start.repository;
+
+import BootBase.Start.domain.Member;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    Member save(Member member);
+    Optional<Member> findById(Long Id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+
+}
